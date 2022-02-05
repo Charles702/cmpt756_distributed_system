1b666e3e6707464d54cd29133767825107944b0c
Chen Zhu <cza130@sfu.ca>
2022-02-04 22:02:01 -0800

    fix the bug in assignment4

--- s2/v1/app.py
+++ s2/v1/app.py
@@ -127,7 +127,7 @@ def delete_song(music_id):
 @bp.route('/test', methods=['GET'])
 def test():
     # This value is for user scp756-221
-    if ('6cbd353eaadbc61c35132838888c136e96e31f10643fb2b472753b1acfb36e58' !=
+    if ('358703958e5635a4f031649568d2e21ac2bbfe94afb54f03c9bbda9b9444adfb' !=
             ucode):
         raise Exception("Test failed")
     return {}
